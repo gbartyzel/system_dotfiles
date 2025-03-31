30e013e976756e9192d739e5343df80f74e52472
Grzegorz Bartyzel <gbartyzel@hotmail.com>
2025-03-31 21:27:15 +0200

    Minor update

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 # Install oh-my-zsh
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-ln -sf ${SCRIPT_DIR}/.zshrc ~/.zshrc 
-ln -sf ${SCRIPT_DIR}/.p10k.zsh ~/.p10k.zsh
-ln -sf ${SCRIPT_DIR}/.tmux.conf ~/.tmux.conf 
-ln -sf ${SCRIPT_DIR}/.config/nvim ~/.config/nvim 
+ln -s ${SCRIPT_DIR}/.zshrc ~/.zshrc
+ln -s ${SCRIPT_DIR}/.p10k.zsh ~/.p10k.zsh
+ln -s ${SCRIPT_DIR}/.tmux.conf ~/.tmux.conf
+ln -s ${SCRIPT_DIR}/.config/nvim ~/.config/nvim
